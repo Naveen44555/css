53a6a4310b78930d846aeb8f3e855a5353a40f39
Naveen44555 <youremail@example.com>
2025-09-06 10:43:51 +0530

    first commit

---
+++ day6/p.py
@@ -0,0 +1,10 @@
+# # nums=[11, 22, 33, 44, 55, 66]
+# # result=[nums[i] for i in range (0,len(nums),2 )if nums[i]%2 !=0]
+# # print(result)
+
+# num=[12, -7,5,-3,9,-15]
+# cl=[0 if n<0 else n for n in num]
+# print(cl)
+
+
+
